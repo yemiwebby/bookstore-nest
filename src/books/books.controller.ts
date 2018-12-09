38556a1cd070d05de28398e84716ae627d90839d
yemiwebby <yemiwebby@gmail.com>
2018-12-09 05:11:42 +0100

    added more service

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Query, Delete, Put } from '@nestjs/common';
+import { BooksService } from './books.service';
+import { CreateBookDTO } from './dto/create-book.dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+    constructor(private booksService: BooksService) { }
+
+    @Get()
+    async getBooks() {
+        const books = await this.booksService.getBooks();
+        return books;
+    }
+
+    @Get(':bookID')
+    async getBook(@Param('bookID') bookID) {
+        const book = await this.booksService.getBook(bookID);
+        return book;
+    }
+
+    @Post()
+    async addBook(@Body() createBookDTO: CreateBookDTO) {
+        const book = await this.booksService.addBook(createBookDTO);
+        return book;
+    }
+
+    @Put()
+    async updateBook(@Query() query) {
+        const books = await this.booksService.updateBook(query.bookID);
+        return books;
+    }
+
+    @Delete()
+    async deleteBook(@Query() query) {
+        const books = await this.booksService.deleteBook(query.bookID);
+        return books;
+    }
+}
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
+import { BOOKS } from '../mocks/books.mock';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+    books = BOOKS;
+
+    getBooks(): Promise<any> {
+        return new Promise(resolve => {
+            resolve(this.books);
+        });
+    }
+
+    // getBook(bookID) {
+    //     let id = Number(bookID);
+    //     return this.books.find(book => book.id === id);
+    // }
+
+    getBook(bookID): Promise<any> {
+        let id = Number(bookID);
+        return new Promise(resolve => {
+            const book = this.books.find(book => book.id === id);
+            if (!book) {
+                throw new HttpException('Book does not exist!', 404);
+            }
+            resolve(book);
+        });
+    }
+
+    addBook(book): Promise<any> {
+        return new Promise(resolve => {
+            this.books.push(book);
+            resolve(this.books);
+        });
+    }
+
+    updateBook(bookID): Promise<any> {
+        let id = Number(bookID);
+        return new Promise(resolve => {
+            let index = this.books.findIndex(book => book.id === id);
+            if (index === -1) {
+                throw new HttpException('Book does not exist!', 404);
+            }
+            this.books.splice(1, index);
+            resolve(this.books);
+        });
+    }
+
+    deleteBook(bookID): Promise<any> {
+        let id = Number(bookID);
+        return new Promise(resolve => {
+            let index = this.books.findIndex(book => book.id === id);
+            if (index === -1) {
+                throw new HttpException('Book does not exist!', 404);
+            }
+            this.books.splice(1, index);
+            resolve(this.books);
+        });
+    }
+}
