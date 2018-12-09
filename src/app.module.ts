078c5f4a3ea231cac6a4a69adfc80e9476014162
yemiwebby <yemiwebby@gmail.com>
2018-12-09 03:14:59 +0100

    added the books module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
+  imports: [BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
